ecf3144b1902a6e949aab09db58da15bde01ad87
Bradon je <khaos1004@gmail.com>
2023-11-20 21:47:55 +0900

    header, side, footer

---
+++ koitoon/src/components/layout/root/RootLayout.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/public/header/Header"
+import Side from "@/components/public/side/Side"
+import Footer from "@/components/public/footer/Footer"
+
+const RootLayout = () => {
+    return(
+        <>
+            <Header />
+            <Side />
+            <Footer />
+        </>
+    )
+}
+
+export default RootLayout;
